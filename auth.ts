bbc4c7ca58c9d046bf73646509ee23a0d1b17ae5
daddylunix <ramezjoseph8@gmail.com>
2024-06-16 13:47:13 +0300

    jobspire init

---
+++ auth.ts
@@ -0,0 +1,47 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import prisma from './utils/db';
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  pages: { 
+    // signIn: '/login'
+    // change test so user creates new page.
+    // newUser: '/test'
+  },
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  secret:process.env.AUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  trustHost: true,
+  callbacks: {
+    async jwt({ token, user }) {
+        if(user) {
+            token.id = user.id
+        }
+      return token
+    },
+    async session({ token, session }) {
+      if(session.user && token.sub) {
+        session.user.id = token.sub
+      } 
+      return session;
+    }
+  },
+  
+})
