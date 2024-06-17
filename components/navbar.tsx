d2b447a9d88ba54eedb10d0c45ea047b0647dde2
daddylunix <ramezjoseph8@gmail.com>
2024-06-17 20:00:10 +0300

    UI :')

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,7 @@ export function Navigation(props:any) {
     <div className="flex w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-          <Link href="/" className="flex items-center gap-2 text-lg font-bold md:text-base">
+          <Link href="/" className="flex items-center gap-2 text-lg font-bold text-lg">
             Jobspire
           </Link>
           <Link href="/" className="text-foreground transition-colors hover:text-foreground font-semibold">
