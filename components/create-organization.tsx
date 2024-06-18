4bb555bccf18d08d5cd6608f985b79fdb6b2be58
daddylunix <ramezjoseph8@gmail.com>
2024-06-18 15:13:15 +0300

    organization creation

--- components/create-organization.tsx
+++ components/create-organization.tsx
@@ -5,20 +5,56 @@ import { Separator } from "./ui/separator";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
+import { motion } from "framer-motion"
 
 export function CreateOrganization() {
     const [ name, setName ] = useState<string>();
+    const [ step, setStep ] = useState<number>(1);
     return (
         <>
         <div className="grid gap-4">
               <div className="-mt-1">
                 <Separator />
               </div>
-                <Label>Organization Name</Label>
-                <Input placeholder="Jobspire"/>
-                <Label>Organization Slug</Label>
-                <Input placeholder="jobspire.astralics.com"/>
-              <Button type="submit">Create</Button>
+              {
+                step === 1 && (
+                    <motion.div 
+                    className="w-full grid gap-4"
+                    initial= {{opacity: 0}}
+                    animate= {{opacity: 1}}
+                    >
+                    <Label>Organization Name</Label>
+                    <Input placeholder="Jobspire"/>
+                    <Button type="submit" onClick={(() => setStep(step + 1))}>Next</Button>
+                    </motion.div>
+                )
+              }
+              {
+                step === 2 && (
+                    <motion.div 
+                    className="w-full grid gap-4"
+                    initial= {{opacity: 0}}
+                    animate= {{opacity: 1}}
+                    >
+                    <Label>Organization Slug</Label>
+                    <Input placeholder="slug.jobspire.co"/>
+                    <Button className="w-full" type="submit" onClick={(() => setStep(step + 1))}>Next</Button>
+                    </motion.div>
+                )
+              }
+               {
+                step === 3 && (
+                    <motion.div 
+                    className="w-full grid gap-4"
+                    initial= {{opacity: 0}}
+                    animate= {{opacity: 1}}
+                    >
+                    <Label>Organization Website</Label>
+                    <Input placeholder="jobspire.co"/>
+                    <Button type="submit">Create</Button>
+                    </motion.div>
+                )
+              }
             </div>
         </>
     )
