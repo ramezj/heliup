8578a649fb0318584a571ea31c85fae3efcb32f7
daddylunix <ramezjoseph8@gmail.com>
2024-06-19 13:11:08 +0300

    metadata update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,11 @@ import { Navigation } from "@/components/navbar";
 import { auth } from "@/auth";
 import { Metadata } from "next";
 
+export const metadata:Metadata = {
+  title: "Jobspire",
+  description: "Jobspire, Your one-stop platform for creating and managing custom job boards."
+}
+
 export default async function Page() {
   const session = await auth();
   return (
