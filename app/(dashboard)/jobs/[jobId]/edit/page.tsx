ec05cc43f7001184bd43bc3946faa3d62f3a2f3c
daddylunix <ramezjoseph8@gmail.com>
2024-06-24 14:57:20 +0300

    testing tabs UI shadcn

--- app/(dashboard)/jobs/[jobId]/edit/page.tsx
+++ app/(dashboard)/jobs/[jobId]/edit/page.tsx
@@ -3,6 +3,23 @@ import { Job } from "@prisma/client"
 import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+  } from "@/components/ui/tabs"
+  import { Textarea } from "@/components/ui/textarea";
 
 export const metadata:Metadata = {
     title: "Edit Job",
@@ -19,7 +36,79 @@ export default async function Page({ params }: { params: { jobId: string } }) {
         <h1 className="font-bold text-3xl">Edit Job</h1>
         <Button>Preview Job</Button>
         </div>
-        {job?.job?.title}
+    <Tabs defaultValue="account" className="">
+      <TabsList className="grid w-full grid-cols-4">
+        <TabsTrigger value="account">Title</TabsTrigger>
+        <TabsTrigger value="password">Location</TabsTrigger>
+        <TabsTrigger value="type">Type</TabsTrigger>
+        <TabsTrigger value="content">Content</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when youre done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" defaultValue="@peduarte" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, youll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="content">
+        <Card>
+          <CardHeader>
+            <CardTitle>Content</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when youre done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Describe Your Job</Label>
+              <Textarea id="name" rows={7}/>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
         </>
     )
 }
