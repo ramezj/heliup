854cc54f9765889783ab19f1a48c5b5d41341573
daddylunix <ramezjoseph8@gmail.com>
2024-06-25 23:02:12 +0300

    uI, new component, new stating

--- app/(dashboard)/jobs/[jobId]/edit/page.tsx
+++ app/(dashboard)/jobs/[jobId]/edit/page.tsx
@@ -3,31 +3,7 @@ import { Job } from "@prisma/client"
 import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-  import { Input } from "@/components/ui/input"
-  import { Label } from "@/components/ui/label"
-  import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger,
-  } from "@/components/ui/tabs"
-  import { Textarea } from "@/components/ui/textarea";
-  import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-
+import EditJobTabs from "@/components/edit-job-tabs";
 
 export const metadata:Metadata = {
     title: "Edit Job",
@@ -36,7 +12,6 @@ export const metadata:Metadata = {
 
 export default async function Page({ params }: { params: { jobId: string } }) {
     const job = await getJobById(params.jobId);
-    console.log(job);
     if(job?.error) { redirect('/jobs') }
     return (
         <>
@@ -44,88 +19,7 @@ export default async function Page({ params }: { params: { jobId: string } }) {
         <h1 className="font-bold text-3xl">Edit Job</h1>
         <Button size={"sm"}>Preview Job</Button>
         </div>
-    <Tabs defaultValue="details" className="">
-      <TabsList className="grid w-full grid-cols-4">
-        <TabsTrigger value="details">Details</TabsTrigger>
-        <TabsTrigger value="description">Description</TabsTrigger>
-        <TabsTrigger value="type">Type</TabsTrigger>
-        <TabsTrigger value="content">Content</TabsTrigger>
-      </TabsList>
-      <TabsContent value="details">
-        <Card>
-          <CardHeader>
-            <CardTitle>Job Details</CardTitle>
-            <CardDescription>
-              Make changes to your job details here, Click save when youre done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Title</Label>
-              <Input id="name" placeholder="Product Manager"/>
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Type</Label>
-                <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Full-Time" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="FULLTIME">Full-Time</SelectItem>
-                  <SelectItem value="PARTTIME">Part-Time</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="description">
-        <Card>
-          <CardHeader>
-            <CardTitle>Job Description</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, youll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="content">
-        <Card>
-          <CardHeader>
-            <CardTitle>Content</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when youre done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Describe Your Job</Label>
-              <Textarea id="name" rows={7}/>
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
+        <EditJobTabs job={job?.job as Job} />
         </>
     )
 }
---
+++ components/edit-job-tabs.tsx
@@ -0,0 +1,119 @@
+"use client"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import { Button } from "./ui/button"
+  import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+  } from "@/components/ui/tabs"
+  import { Textarea } from "@/components/ui/textarea";
+  import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+  import { Job } from "@prisma/client"
+  import { useState } from "react"
+
+export default function EditJobTabs({ job }: { job: Job }) {
+    const [ loading, setLoading ] = useState<Boolean>(false);
+    const [ title, setTitle ] = useState<string>(job.title);
+    return (
+        <>
+        <Tabs defaultValue="details" className="">
+      <TabsList className="grid w-full grid-cols-4">
+        <TabsTrigger value="details">Details</TabsTrigger>
+        <TabsTrigger value="description">Description</TabsTrigger>
+        <TabsTrigger value="type">Type</TabsTrigger>
+        <TabsTrigger value="content">Content</TabsTrigger>
+      </TabsList>
+      <TabsContent value="details">
+        <Card>
+          <CardHeader>
+            <CardTitle>Job Details</CardTitle>
+            <CardDescription>
+              Make changes to your job details here, Click save when youre done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Title</Label>
+              <Input placeholder="Product Manager" value={title} onChange={((e) => {setTitle(e.target.value)})}/>
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Type</Label>
+                <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Full-Time" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="FULLTIME">Full-Time</SelectItem>
+                  <SelectItem value="PARTTIME">Part-Time</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="description">
+        <Card>
+          <CardHeader>
+            <CardTitle>Job Description</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, youll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="content">
+        <Card>
+          <CardHeader>
+            <CardTitle>Content</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when youre done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Describe Your Job</Label>
+              <Textarea id="name" rows={7}/>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+        </>
+    )
+}
