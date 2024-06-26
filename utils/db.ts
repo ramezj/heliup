3008279a02265b36364cbd39568fb1c6e72d8813
daddylunix <ramezjoseph8@gmail.com>
2024-06-26 17:43:20 +0300

    FIXED THIS FINALLY!

--- utils/db.ts
+++ utils/db.ts
@@ -1,27 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
-import { performance } from "perf_hooks";
-import * as util from "util";
 
-const prismaClientSingleton = () => {
-  const libsql = createClient({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN
-  })
-  const adapter = new PrismaLibSQL(libsql)
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`
+})
 
-  return new PrismaClient({ adapter })
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const adapter = new PrismaLibSQL(libsql)
+const prisma = new PrismaClient({ adapter })
+export default prisma;
