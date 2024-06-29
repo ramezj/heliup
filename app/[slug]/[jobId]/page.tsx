d8ed4d7aafc6885e9febe8ce24742af8e15b2fcb
daddylunix <ramezjoseph8@gmail.com>
2024-06-29 12:23:01 +0300

    some smol ui updates

--- app/[slug]/[jobId]/page.tsx
+++ app/[slug]/[jobId]/page.tsx
@@ -1,14 +1,20 @@
 import { getJobById } from "@/server-actions/jobs/get-job"
 import { Job } from "@prisma/client"
 import { redirect } from "next/navigation";
+import { RenderContent } from "@/components/render-content";
 
 export default async function Page({ params }: { params: { jobId: string } }) {
     const job = await getJobById(params.jobId);
-    console.log(job);
     if(job?.error) { redirect('/jobs') }
+    console.log(job.job?.content)
     return (
         <>
-        {JSON.stringify(job?.job)}
+        <div className="w-full flex flex-col items-center text-center p-8">
+            <h1 className="font-bold text-2xl">{job.job?.title}</h1>
+            <div>
+                <RenderContent content={job.job?.content!} />
+            </div>
+        </div>
         </>
     )
 }
---
+++ components/render-content.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { EditorContent, useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import React, { useEffect, useState } from 'react'
+export function RenderContent({ content } : { content: string}) {
+    const [editable, setEditable] = useState(false)
+    const editor = useEditor({
+        editable,
+        content: content,
+        extensions: [StarterKit],
+  })
+    useEffect(() => {
+        if (!editor) {
+        return undefined
+        }
+        editor.setEditable(editable)
+    }, [editor, editable])
+    if (!editor) { return null }
+    return (
+        <>
+        <EditorContent editor={editor} />
+        </>
+    )
+}
