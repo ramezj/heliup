a0271976f167ade121ea519b9e98d64ba6f8e966
daddylunix <ramezjoseph8@gmail.com>
2024-07-09 12:50:03 +0300

    testing something

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,7 +16,7 @@ export function Navigation(props:any) {
   const path = useLastPathSegment();
   return (
     <div className="flex w-full flex-col">
-      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-3">
+      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-3 z-50">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link href="/" className="flex items-center gap-2 text-lg font-bold">
           hirehollo
