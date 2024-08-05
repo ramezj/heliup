63352d4bee8854efe05862bde1ca308b8d9332be
daddylunix <ramezjoseph8@gmail.com>
2024-08-05 15:08:35 +0300

    /applicants/[jobId]

--- app/(dashboard)/applicants/[jobId]/page.tsx
+++ app/(dashboard)/applicants/[jobId]/page.tsx
@@ -1,7 +1,38 @@
-export default function Page() {
+import { Applicant, Job } from "@prisma/client"
+import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Metadata } from "next";
+import EditJobTabs from "@/components/edit-job";
+import Link from "next/link";
+import { Trash } from "lucide-react";
+import { ApplicantCard } from "@/components/applicants-card";
+import { getJobApplicants } from "@/server-actions/applicants/get-job-applicants";
+
+export const metadata:Metadata = {
+    title: "View Applicants",
+    description: "Edit Your Job."
+}
+
+export default async function Page({ params }: { params: { jobId: string } }) {
+    const job = await getJobApplicants(params.jobId);
+    if(job?.error || job?.job === null) { redirect('/jobs') }
+    console.log(job.job?.applicants)
     return (
         <>
-        List of all applicants goes here!
+        <div className="flex justify-between items-center w-full">
+        <h1 className="font-bold text-3xl">Applicants</h1>
+        </div>
+        <div className="flex flex-col gap-4">
+        {
+            job.job?.applicants.map((applicant:Applicant) => {
+                return (
+                    <div className="relative" key={applicant.id}>
+                    <ApplicantCard applicant={applicant} />
+                    </div>
+                )
+            })
+        }
+        </div>
         </>
     )
 }
