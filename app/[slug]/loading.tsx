86ef6fd663e8aa1de39161efb511a13aefb2639b
daddylunix <ramezjoseph8@gmail.com>
2024-08-08 11:26:42 +0300

    loading

--- app/[slug]/loading.tsx
+++ app/[slug]/loading.tsx
@@ -5,7 +5,6 @@ export default function Loading() {
     return (
         <div className="w-full flex flex-col items-center text-center p-8">
             <Skeleton className="h-4 w-[250px]" />
-            <Skeleton className="h-4 w-[250px]" />
             <br />
             <div className="flex flex-col gap-4 sm:w-1/2 w-full">
             <LoadingJob />
