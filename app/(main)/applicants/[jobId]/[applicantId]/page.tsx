1506a6e709d39421dd13c641257bf462f3b4a11f
daddylunix <ramezjoseph8@gmail.com>
2024-08-09 16:13:35 +0300

    working on /applicants/jobId/applicantId

--- app/(main)/applicants/[jobId]/[applicantId]/page.tsx
+++ app/(main)/applicants/[jobId]/[applicantId]/page.tsx
@@ -3,6 +3,8 @@ import { Metadata } from "next"
 import { redirect } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
 
 export const metadata:Metadata = {
     title: "View Applicants",
@@ -18,9 +20,19 @@ export default async function Page({ params }: { params: { applicantId: string }
         <h1 className="font-bold text-3xl tracking-tight">View Applicant</h1>
         </div>
         <div className="space-y-2">
-        <h1 className="text-xl font-medium">{applicant.applicant?.first_name} {applicant.applicant?.last_name}</h1>
-        <Badge className="rounded-md">{applicant.applicant?.email}</Badge>
-        <h1>{applicant.applicant?.motivation}</h1>
+        <Label>Full Name</Label>
+        <Input value={`${applicant.applicant?.first_name} ${applicant.applicant?.last_name}`} readOnly />
+        {/* <h1 className="text-xl font-medium">{applicant.applicant?.first_name} {applicant.applicant?.last_name}</h1> */}
+        </div>
+        <div className="space-y-2">
+        <Label>Email Address</Label>
+        <Input value={applicant.applicant?.email} readOnly />
+        </div>
+        <div className="space-y-2"> 
+        <Label className=" text-lg">Motivation Letter</Label>
+        <div className="">
+        <Textarea value={applicant.applicant?.motivation} readOnly />
+        </div>
         </div>
         </>
     )
