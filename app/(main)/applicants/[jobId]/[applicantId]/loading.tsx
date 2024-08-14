9009a4c8422933ef46f0e08cf96793df9dab7ab3
daddylunix <ramezjoseph8@gmail.com>
2024-08-14 15:04:59 +0300

    testing new loading..

--- app/(main)/applicants/[jobId]/[applicantId]/loading.tsx
+++ app/(main)/applicants/[jobId]/[applicantId]/loading.tsx
@@ -1,5 +1,6 @@
 import { Skeleton } from "@/components/ui/skeleton"
 import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
 
 export default function Loading() {
     return (
@@ -7,7 +8,7 @@ export default function Loading() {
         <div className="flex justify-between items-center w-full">
         <h1 className="font-bold text-3xl tracking-tight">View Applicant</h1>
         </div>
-        <div className="space-y-4"> 
+        {/* <div className="space-y-4"> 
         <div className="space-y-2">
         <Skeleton className="w-24 h-6" />
         <Skeleton className="w-full h-10" />
@@ -35,7 +36,14 @@ export default function Loading() {
         </Button>
         </div>
         </div>
+        </div> */}
+        <div className="w-full h-full items-center flex flex-col justify-center">
+        <Loader2 className="size-8 animate-spin text-blue-400" />
         </div>
+      {/* </div>
+        <div className="w-full h-fullitems-center justify-center content-center flex place-content-center">
+        <Loader2 className="size-6 animate-spin" />
+        </div> */}
         </>
     )
 }
