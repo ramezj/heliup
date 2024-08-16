617e112bc2c4651ffe17769abede33a2fd5f7d80
daddylunix <ramezjoseph8@gmail.com>
2024-08-17 02:05:18 +0300

    working on navbar for [slug]

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -3,7 +3,13 @@ import { Toggle } from "@/components/toggle"
 export default function Layout({children, params, req} : any) {
     return (
         <>
-        <div className="w-full border-b border-0 h-16 sticky">
+        <div className="w-full border-b border-0 h-16 sticky text-center justify-between flex items-center px-6">
+        <div className="flex">
+            <h1>website</h1>
+        </div>
+        <div className="flex">
+            <h1>website 2</h1>
+        </div>
         </div>
         {children}
         </>
