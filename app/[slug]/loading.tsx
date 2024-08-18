814ab294a5c9fa8b7e0989f5b4e2702fead8e6f3
daddylunix <ramezjoseph8@gmail.com>
2024-08-18 21:24:56 +0300

    now trying min-h-screen

--- app/[slug]/loading.tsx
+++ app/[slug]/loading.tsx
@@ -7,13 +7,13 @@ import Link from "next/link";
 export default function Loading() {
     const params = useParams<{ slug: string}>()
     return (
-        <div className="h-screen">
+        <div className="min-h-screen">
         <div className="w-full border-b border-0 h-16 sticky text-center justify-between flex items-center px-6 bg-black">
         <div className="flex">
         <Link className="font-bold text-sm sm:text-base" href='/'>{params.slug}</Link>
         </div>
         </div>
-        <div className="w-full h-full items-center flex flex-col justify-center content-center place-content-center">
+        <div className="w-full h-screen items-center flex flex-col justify-center content-center place-content-center">
         <Loader2 className="size-8 animate-spin text-gray-200" />
         </div>
         </div>
