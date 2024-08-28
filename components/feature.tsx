4668ad16c8b0863be316f01f70cfcd8af734c707
daddylunix <ramezjoseph8@gmail.com>
2024-08-28 16:07:20 +0300

    ..

--- components/feature.tsx
+++ components/feature.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 import { Calendar } from "@/components/ui/calendar";
 import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
 import Marquee from "@/components/magicui/marquee";
+import { Input } from "./ui/input";
+import { LandingPageJobCard } from "./job-card";
 
 const files = [
   {
@@ -125,34 +127,34 @@ export function BentoDemo() {
         cta="Start Hiring"
         background={
         <>
-        <Calendar
-        mode="single"
-        selected={new Date(2022, 4, 11, 0, 0, 0)}
-        className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"
-        />
+        <Input className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out group-hover:scale-105" value={"demo.heliup.xyz"} disabled />
         </>
         }/>
 
 
         <BentoCard key={1} 
         Icon={Users} 
-        name="Applicant Management" 
-        description="Use your own custom domain and attract more applicants." 
+        name="Job Board" 
+        description="Create stunning career pages & start hiring." 
         className="col-span-3 lg:col-span-2"
         href="#"
         cta="Start Hiring"
         background={
-        <Calendar
-        mode="single"
-        selected={new Date(2022, 4, 11, 0, 0, 0)}
-        className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"
-        />}/>
+          <div className="gap-2 flex flex-col absolute right-0 top-10 origin-top rounded-md transition-all duration-300 ease-out group-hover:scale-105">
+          <div className="relative">
+          <LandingPageJobCard title="Software Engineer" type="FULLTIME" location="Remote" />
+          </div>
+          <div className="relative">
+          <LandingPageJobCard title="Software Engineer" type="FULLTIME" location="Remote" />
+          </div>
+          </div>
+          }/>
 
 
         <BentoCard key={1} 
         Icon={FileTextIcon} 
         name="Review All Resumes" 
-        description="Use your own custom domain and attract more applicants." 
+        description="Receive & Review all applicant Resumes." 
         className="col-span-3 lg:col-span-2"
         href="#"
         cta="Start Hiring"
