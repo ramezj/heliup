f7e25370207761ac13cdce3fdd7863c3cf7e4c3e
daddylunix <ramezjoseph8@gmail.com>
2024-08-28 16:23:30 +0300

    dashboard.png test :D

--- components/feature.tsx
+++ components/feature.tsx
@@ -1,119 +1,13 @@
 import { CalendarIcon, FileTextIcon } from "@radix-ui/react-icons";
 import { BellIcon, Link, Share2Icon, Users } from "lucide-react";
 import ShineBorder from "./magicui/shine-border";
-
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Calendar } from "@/components/ui/calendar";
 import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
 import Marquee from "@/components/magicui/marquee";
 import { Input } from "./ui/input";
 import { LandingPageJobCard } from "./job-card";
-
-const files = [
-  {
-    name: "bitcoin.pdf",
-    body: "Bitcoin is a cryptocurrency invented in 2008 by an unknown person or group of people using the name Satoshi Nakamoto.",
-  },
-  {
-    name: "finances.xlsx",
-    body: "A spreadsheet or worksheet is a file made of rows and columns that help sort data, arrange data easily, and calculate numerical data.",
-  },
-  {
-    name: "logo.svg",
-    body: "Scalable Vector Graphics is an Extensible Markup Language-based vector image format for two-dimensional graphics with support for interactivity and animation.",
-  },
-  {
-    name: "keys.gpg",
-    body: "GPG keys are used to encrypt and decrypt email, files, directories, and whole disk partitions and to authenticate messages.",
-  },
-  {
-    name: "seed.txt",
-    body: "A seed phrase, seed recovery phrase or backup seed phrase is a list of words which store all the information needed to recover Bitcoin funds on-chain.",
-  },
-];
-
-const features = [
-  {
-    Icon: FileTextIcon,
-    name: "Save your files",
-    description: "We automatically save your files as you type.",
-    href: "#",
-    cta: "Learn more",
-    className: "col-span-3 lg:col-span-1",
-    background: (
-      <Marquee
-        pauseOnHover
-        className="absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] "
-      >
-        {files.map((f, idx) => (
-          <figure
-            key={idx}
-            className={cn(
-              "relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4",
-              "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-              "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-              "transform-gpu blur-[1px] transition-all duration-300 ease-out hover:blur-none",
-            )}
-          >
-            <div className="flex flex-row items-center gap-2">
-              <div className="flex flex-col">
-                <figcaption className="text-sm font-medium dark:text-white ">
-                  {f.name}
-                </figcaption>
-              </div>
-            </div>
-            <blockquote className="mt-2 text-xs">{f.body}</blockquote>
-          </figure>
-        ))}
-      </Marquee>
-    ),
-  },
-  {
-    Icon: BellIcon,
-    name: "Notifications",
-    description: "Get notified when something happens.",
-    href: "#",
-    cta: "Learn more",
-    className: "col-span-3 lg:col-span-2",
-    background: (
-        <Calendar
-        mode="single"
-        selected={new Date(2022, 4, 11, 0, 0, 0)}
-        className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"
-      />
-    ),
-  },
-  {
-    Icon: BellIcon,
-    name: "Notifications",
-    description: "Get notified when something happens.",
-    href: "#",
-    cta: "Learn more",
-    className: "col-span-3 lg:col-span-2",
-    background: (
-        <Calendar
-        mode="single"
-        selected={new Date(2022, 4, 11, 0, 0, 0)}
-        className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"
-      />
-    ),
-  },
-  {
-    Icon: CalendarIcon,
-    name: "Calendar",
-    description: "Use the calendar to filter your files by date.",
-    className: "col-span-3 lg:col-span-1",
-    href: "#",
-    cta: "Learn more",
-    background: (
-      <Calendar
-        mode="single"
-        selected={new Date(2022, 4, 11, 0, 0, 0)}
-        className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"
-      />
-    ),
-  },
-];
 
 export function BentoDemo() {
   return (
@@ -127,10 +21,9 @@ export function BentoDemo() {
         cta="Start Hiring"
         background={
         <>
-        <Input className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out group-hover:scale-105" value={"demo.heliup.xyz"} disabled />
+        <Input className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_5%,#000_50%)]  group-hover:scale-105" value={"demo.heliup.xyz"} disabled />
         </>
         }/>
-
 
         <BentoCard key={1} 
         Icon={Users} 
@@ -140,8 +33,8 @@ export function BentoDemo() {
         href="#"
         cta="Start Hiring"
         background={
-          <div className="gap-2 flex flex-col absolute right-0 top-10 origin-top rounded-md transition-all duration-300 ease-out group-hover:scale-105">
-          <div className="relative">
+          <div className="gap-2 flex flex-col absolute right-0 top-10 origin-top rounded-md transition-all duration-300 [mask-image:linear-gradient(to_top,transparent_20%,#000_100%)] ease-out group-hover:scale-105">
+          <div className="relative w-full">
           <LandingPageJobCard title="Software Engineer" type="FULLTIME" location="Remote" />
           </div>
           <div className="relative">
@@ -149,7 +42,6 @@ export function BentoDemo() {
           </div>
           </div>
           }/>
-
 
         <BentoCard key={1} 
         Icon={FileTextIcon} 
@@ -159,11 +51,11 @@ export function BentoDemo() {
         href="#"
         cta="Start Hiring"
         background={
-        <Calendar
-        mode="single"
-        selected={new Date(2022, 4, 11, 0, 0, 0)}
-        className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105"
-        />}/>
+          <>
+          <Image src={"/dashboard.png"} width={500} height={500} alt="dashboard" 
+          className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105" />
+          </>
+       }/>
 
         <BentoCard key={1} 
         Icon={Link} 
