cab3da7f51ce3730160b68c831c5fd6d34677382
daddylunix <ramezjoseph8@gmail.com>
2024-09-02 22:07:23 +0300

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={bricolageGrotesque.className}>
+      <link rel="icon" href="/logo.jpg" />
       <body>
         <Toaster />
         <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
