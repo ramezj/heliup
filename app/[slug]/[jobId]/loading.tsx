b4b8865ddf20f061f98ac568dd8c67b9a872469a
daddylunix <ramezjoseph8@gmail.com>
2024-09-02 21:08:59 +0300

    loading.tsx

---
+++ app/[slug]/[jobId]/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from "lucide-react"
+
+export default function Loading() {
+    return (
+        <>
+        <div className="w-full h-screen items-center flex flex-col justify-center">
+        <Loader2 className="size-8 animate-spin text-gray-200" />
+        </div>
+        </>
+    )
+}
