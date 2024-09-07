974f5821cb546d892afdc30f9c89fc24229dd3f6
daddylunix <ramezjoseph8@gmail.com>
2024-09-07 13:19:38 +0300

    new paddle webhook

---
+++ app/api/paddle/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { validateSignature } from "@/utils/validate-signature";
+
+export async function POST(req: NextRequest) {
+    const signature = req.headers.get("Paddle-Signature");
+    const body = await req.text();
+    // const isValid = await validateSignature(signature!, body, process.env.PADDLE_SECRET!);
+    // if(!isValid) {
+    //     return NextResponse.json({
+    //         ok:false,
+    //         message:null,
+    //         error:"Invalid Signature"
+    //     });
+    // };
+    const parsedbody = await JSON.parse(body);
+    console.log(parsedbody);
+    return NextResponse.json({
+        ok:true,
+        error: null,
+        message:parsedbody
+    })
+}
