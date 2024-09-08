85ed2155ba72e4981e552e0439800ad07866f1c1
daddylunix <ramezjoseph8@gmail.com>
2024-09-08 20:06:43 +0300

    changing some features and changing pictures of bentogrid

--- components/feature.tsx
+++ components/feature.tsx
@@ -8,14 +8,15 @@ import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
 import Marquee from "@/components/magicui/marquee";
 import { Input } from "./ui/input";
 import { LandingPageJobCard } from "./job-card";
+import { ShieldCheck } from "lucide-react";
 
 export function BentoDemo() {
   return (
     <BentoGrid>
         <BentoCard key={1} 
-        Icon={Link} 
-        name="Custom Domain" 
-        description="Use your own custom domain and attract more applicants." 
+        Icon={ShieldCheck} 
+        name="Custom Branding" 
+        description="Bring your own branding" 
         className="col-span-3 lg:col-span-1"
         href="#"
         cta="Start Hiring"
@@ -25,25 +26,21 @@ export function BentoDemo() {
         </>
         }/>
 
-        <BentoCard key={1} 
+        <BentoCard key={2} 
         Icon={Users} 
-        name="Job Board" 
-        description="Create stunning career pages & start hiring." 
+        name="Custom Job Board" 
+        description="Create stunning career pages" 
         className="col-span-3 lg:col-span-2"
         href="#"
         cta="Start Hiring"
         background={
-          <div className="gap-2 flex flex-col absolute right-0 top-10 origin-top rounded-md transition-all duration-300 [mask-image:linear-gradient(to_top,transparent_20%,#000_100%)] ease-out group-hover:scale-105">
-          <div className="relative w-full">
-          <LandingPageJobCard title="Software Engineer" type="FULLTIME" location="Remote" />
-          </div>
-          <div className="relative">
-          <LandingPageJobCard title="Software Engineer" type="FULLTIME" location="Remote" />
-          </div>
-          </div>
+          <>
+          <Image src={"/demo.png"} width={500} height={500} alt="dashboard" 
+          className="absolute right-0 top-10 origin-top rounded-md border transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] group-hover:scale-105" />
+          </>
           }/>
 
-        <BentoCard key={1} 
+        <BentoCard key={3} 
         Icon={FileTextIcon} 
         name="Review All Resumes" 
         description="Receive & Review all applicant Resumes." 
@@ -57,7 +54,7 @@ export function BentoDemo() {
           </>
        }/>
 
-        <BentoCard key={1} 
+        <BentoCard key={4} 
         Icon={Link} 
         name="Custom Domain" 
         description="Use your own custom domain and attract more applicants." 
