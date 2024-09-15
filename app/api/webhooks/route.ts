ff3819bb007be7732b2c9ec56b3fc37ac8983cc4
daddylunix <ramezjoseph8@gmail.com>
2024-09-15 19:21:31 +0300

    changed req.text -> req.json

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { validateSignature } from "@/utils/validate-signature";
 
 export async function POST(req: NextRequest) {
-    const body = await req.text();
+    const body = await req.json();
     console.log(body);
     return NextResponse.json({
         ok:true,
