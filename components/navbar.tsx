000573db8c1a93e7d04a94c0a3f897efbe6770be
daddylunix <ramezjoseph8@gmail.com>
2024-10-06 19:16:55 +0300

    navbar change from get started to login

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -93,7 +93,7 @@ export function Navigation(props:any) {
             <>
                   <Button variant="default" asChild className="duration-200">
                   <Link href='/auth'>
-                  Start Hiring <i>&nbsp;- It&apos;s Free</i>
+                  Login
                   </Link>
                   </Button>
             </>
