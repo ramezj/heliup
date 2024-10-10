dc6caf5ffdcb1bb4f952259952ca46551419ebe2
daddylunix <ramezjoseph8@gmail.com>
2024-10-10 09:54:34 +0300

    prisma schema update, testing out subscriptionssss

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,10 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
 import { validateSignature } from "@/utils/validate-signature";
+import prisma from "@/utils/db";
 
 export async function POST(req: NextRequest) {
     const bodyText = await req.text();
     const parsedbody = new URLSearchParams(bodyText);
     const body = Object.fromEntries(parsedbody.entries());
+    const resource_name = body?.resource_name;
+    const subscription_id = body?.subscription_id;
+    const email = body?.email;
+
+    // New Subscription
+    if(resource_name == "sale") {
+        const user = await prisma.user.update({
+            where: {
+                email: email,
+            },
+            data: {
+                isPremium: true,
+                subscription_id: subscription_id
+            }
+        })
+    }
+    
+    // Cancellation
+    if(resource_name == "cancellation") {
+        const user = await prisma.user.update({
+            where: {
+                email: email
+            },
+            data: {
+                isPremium: false,
+            }
+        })
+    }
     console.log(body);
     return NextResponse.json({
         ok:true,
