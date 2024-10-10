60a36c3651c984e0a7da604ebfd8c98898d64512
daddylunix <ramezjoseph8@gmail.com>
2024-10-10 08:48:18 +0300

    trying something in webhooks

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { validateSignature } from "@/utils/validate-signature";
 
 export async function POST(req: NextRequest) {
-    const body = await req.text();
+    const bodyText = await req.text();
+    const parsedbody = new URLSearchParams(bodyText);
+    const body = Object.fromEntries(parsedbody.entries());
     console.log(body);
-    const parsedbody = new URLSearchParams(body);
-    console.log(parsedbody);
     return NextResponse.json({
         ok:true,
         error: null,
-        message:body
+        message:bodyText
     })
 }
