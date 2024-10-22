dccd933e90ba443c2bf0cc0835132ad7d39ed0f2
daddylunix <ramezjoseph8@gmail.com>
2024-10-22 09:58:51 +0300

    fucking around with shadcn's sidebar :D

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,10 +1,13 @@
+"use client"
 import * as React from "react"
-
+import { usePathname } from "next/navigation"
 import { SearchForm } from "@/components/search-form"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem  } from "./ui/dropdown-menu"
 import { VersionSwitcher } from "@/components/version-switcher"
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -14,29 +17,36 @@ import {
   SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar"
+import { User2, ChevronUp } from "lucide-react"
 
 // This is sample data.
 const data = {
   versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
   navMain: [
     {
-      title: "Getting Started",
+      title: "Building Your Application",
       url: "#",
       items: [
         {
-          title: "Installation",
+          title: "Routing",
           url: "#",
         },
         {
-          title: "Project Structure",
+          title: "Data Fetching",
           url: "#",
+          isActive: true,
         },
+        {
+          title: "Rendering",
+          url: "#",
+        }
       ],
     }
   ],
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const path = usePathname();
   return (
     <Sidebar {...props}>
       <SidebarHeader>
@@ -47,23 +57,64 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         {/* We create a SidebarGroup for each parent. */}
-        {data.navMain.map((item) => (
-          <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {item.items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        ))}
+        <SidebarGroup>
+        <SidebarMenu>
+          <SidebarMenuItem>
+          <SidebarMenuButton asChild isActive={false}>
+            <a href='/'>Home</a>
+          </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+          <SidebarMenuButton asChild isActive={false}>
+            <a href='/'>Jobs</a>
+          </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+          <SidebarMenuButton asChild isActive={false}>
+            <a href='/'>Applicants</a>
+          </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+          <SidebarMenuButton asChild isActive={false}>
+            <a href='/'>Billing</a>
+          </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+          <SidebarMenuButton asChild isActive={false}>
+            <a href='/'>Settings</a>
+          </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton size={"default"}>
+                    <User2 /> Username
+                    <ChevronUp className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width]"
+                >
+                  <DropdownMenuItem>
+                    <span>Account</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <span>Billing</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <span>Sign out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   )
