5672cfc5222c689b6978b9f4c3841aab3aeeaa9f
daddylunix <ramezjoseph8@gmail.com>
2024-11-08 13:29:30 +0200

    bg-background on navigation menu content.

--- components/nav-menu-demo.tsx
+++ components/nav-menu-demo.tsx
@@ -58,9 +58,9 @@ export function NavigationMenuDemo() {
       <NavigationMenuList>
         <NavigationMenuItem>
           <NavigationMenuTrigger>Features</NavigationMenuTrigger>
-          <NavigationMenuContent>
+          <NavigationMenuContent className="bg-background">
             <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              {/* <li className="row-span-3">
+              <li className="row-span-3">
                 <NavigationMenuLink asChild>
                   <a
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
@@ -76,7 +76,7 @@ export function NavigationMenuDemo() {
                     </p>
                   </a>
                 </NavigationMenuLink>
-              </li> */}
+              </li>
               <ListItem href="/docs" title="Introduction">
                 Re-usable components built using Radix UI and Tailwind CSS.
               </ListItem>
